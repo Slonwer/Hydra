983b32298f71df18a623b7c6dd94f77603b6fe51
Slackwert <ioserro53@Gmail.com>
2024-03-28 21:36:32 -0300

    function

---
+++ Vunerabilidade/function.js
@@ -0,0 +1,12 @@
+function confirmacaoExclusaoUser(id){
+    var resposta = confirm("Deseja remover esse registro?");
+    if (resposta == true) {
+		  window.location.href = "delete.php?id=" + id;
+    }
+}
+function var(nem){
+	var nome = (idade,nome,);
+	if (nome == true) {
+		else (nome == false); 
+	}
+}
